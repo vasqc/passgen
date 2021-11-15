aa2a19d12b7838a2e3bec3d75736161a4e85e29c
vasqc <90007988+vasqc@users.noreply.github.com>
2021-11-14 20:36:50 -0500

    final product

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
 // Assignment code here
-var length = parseInt(prompt ("How many Character would you like? Choose between 8 and 26"), 10);
-if ((length >= 8) && (length <= 26)) {
-     var upperCaseLetters = confirm("Would you like to include Uppercase Letters?");
-     if (upperCaseLetters === true) {
-       
-      
-     } else {
-       alert("You press Cancel");
-     }
-     var lowerCaseLetters = confirm("Would you like to include Lowercase Letters?");
-     if (lowerCaseLetters === true) {
-       
-     } else {
-       alert("You press Cancel");
-     }
-     var symbols = confirm("would you like to include symbols?");
-     if (symbols === true) {
-       
-     } else {
-       alert("You press Cancel");
-     }
-     var numbers = confirm("Would you like to include numbers?");
-      if (numbers === true) {
-       
-      }
-var password =""; 
-for (var i = 0; i < length; i++) {
-  var character = [getRandomSymbol(), getRandomNumber(), getRandomUpper(), getRandomLower(),];
-password = password + character;
-}
-var passwordText = document.querySelector("#password");
-passwordText.value = password;
-  };
+function generatePassword() {
+  var length = parseInt(prompt("How many Character would you like? Choose between 8 and 26"), 10);
+  if ((length >= 8) && (length <= 26)) {
+    var numberOfCharacterTypes = 0
+    var upperCaseLetters = confirm("Would you like to include Uppercase Letters?");
+    if (upperCaseLetters === true) {
+      numberOfCharacterTypes += 1
+    } else {
+      alert("You are skipping Uppercase letters.");
+    }
+    var lowerCaseLetters = confirm("Would you like to include Lowercase Letters?");
+    if (lowerCaseLetters === true) {
+      numberOfCharacterTypes += 1
+    } else {
+      alert("You are skipping Lowercase letters.");
+    }
+    var symbols = confirm("would you like to include symbols?");
+    if (symbols === true) {
+      numberOfCharacterTypes += 1
+    } else {
+      alert("You are skipping Symbols.");
+    }
+    var numbers = confirm("Would you like to include numbers?");
+    if (numbers === true) {
+      numberOfCharacterTypes += 1
+    } else {
+      alert("You are skipping Numbers.")
+    }
+    if (numberOfCharacterTypes === 0) {
+      alert("You need to choose at least one character type. Please try again.");
+      return "";
+    }
+    var password = "";
 
+    var character = [];
+
+    for (var i = 0; i < length; i++) {
+      if (upperCaseLetters)
+        character.push(getRandomUpper())
+      if (lowerCaseLetters)
+        character.push(getRandomLower())
+      if (numbers)
+        character.push(getRandomNumber())
+      if (symbols)
+        character.push(getRandomSymbol())
+
+      //var character = [getRandomSymbol(), getRandomNumber(), getRandomUpper(), getRandomLower(),];
+      var randomCharacter = character[Math.floor(Math.random() * character.length)]
+      password = password + randomCharacter;
+    }
+    return password  
+  };
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
-
-
 //create generate password element
 //generate password function
 
-     
-     
-var randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-};
+
 //generator functions
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -65,16 +74,17 @@ function getRandomNumber() {
 function getRandomSymbol() {
   const symbols = "!@#$%^&*()";
   return symbols[Math.floor(Math.random() * symbols.length)];
-} 
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
+
 
